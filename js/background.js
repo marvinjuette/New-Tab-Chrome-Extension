135b40fa615fbbc8c5755508e3250b59aeda9353
Nivram710 <juettemarvin@gmail.com>
2018-10-16 22:42:19 +0200

    Added Comments

--- js/background.js
+++ js/background.js
@@ -1,5 +1,10 @@
+// debug output
 if (debugMode) console.log("Debug: background() called");
+
+// choose random background
 let randomNumber = Math.floor(Math.random() * numberOfBackgroundImages) + 1;
 if (randomNumber < 10) randomNumber = "0" + randomNumber;
 document.body.style.backgroundImage = "url(images/bg" + randomNumber + ".png)";
+
+// debug output
 if (debugMode) console.log("Debug: image=bg" + randomNumber + ".png");
