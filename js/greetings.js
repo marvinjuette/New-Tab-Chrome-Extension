b18f6c878d0a8a8bf6e3ef88ff3d91440e8a01a0
Nivram710 <juettemarvin@gmail.com>
2018-10-16 22:45:20 +0200

    Added name const

--- js/greetings.js
+++ js/greetings.js
@@ -2,8 +2,8 @@
 if (debugMode) console.log("Debug: greetings() called");
 
 // choose greeting after time
-if (h >= 4 && h < 12) document.getElementById('period').innerText = "morning,";
-else if (h >= 12 && h < 14) document.getElementById('period').innerText = "day,";
-else if (h >= 14 && h < 18) document.getElementById('period').innerText = "afternoon,";
-else if (h >= 18 && h < 24) document.getElementById('period').innerText = "evening,";
-else document.getElementById('period').innerText = "night,";
+if (h >= 4 && h < 12) document.getElementById('period').innerHTML = "morning, " + name + ".";
+else if (h >= 12 && h < 14) document.getElementById('period').innerHTML = "day, " + name + ".";
+else if (h >= 14 && h < 18) document.getElementById('period').innerHTML = "afternoon, " + name + ".";
+else if (h >= 18 && h < 24) document.getElementById('period').innerHTML = "evening, " + name + ".";
+else document.getElementById('period').innerHTML = "night, " + name + ".";
