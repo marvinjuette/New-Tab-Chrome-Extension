8f7cb19ff6cca1afd0fe160289d1c9bb0a85b808
Nivram710 <juettemarvin@gmail.com>
2018-10-16 22:40:10 +0200

    Added Comments

--- js/const.js
+++ js/const.js
@@ -1,16 +1,24 @@
+// personal options
+const name = "Marvin";
+const birthday = new Date(2001, 9, 7); //Jahr, Monat (Januar = 0, ..., Dezember = 11), Tag
+
+// developer options
 const debugMode = true;
 const numberOfBackgroundImages = 4;
-const birthday = new Date(2001, 9, 7); //Jahr, Monat (Januar = 0, ..., Dezember = 11), Tag
+
 const date = new Date();
 let h = date.getHours();
 let m = date.getMinutes();
 
 const quotes = [
+// current date/time
+// quotes,
     'What is your Focus today?',
     'Have a great day.',
     'Enjoy your time.'
 ];
 
+// quotes for birthday
 const quotesBirthday = [
     'Happy Birthday!',
     'All the best for your birthday.',
