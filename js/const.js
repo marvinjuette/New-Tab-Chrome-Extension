73052e3d195de0c76d3646a2036cc6405abe12a0
Nivram710 <contact@nivram710.de>
2018-11-13 11:01:48 +0100

    Turned off debugMode

--- js/const.js
+++ js/const.js
@@ -3,12 +3,12 @@ const name = "Marvin";
 const birthday = new Date(2001, 9, 7); //Jahr, Monat (Januar = 0, ..., Dezember = 11), Tag
 
 // developer options
-const debugMode = true;
+const debugMode = false;
 const numberOfBackgroundImages = 4;
 
-const date = new Date();
-let h = date.getHours();
-let m = date.getMinutes();
+const date = new Date()
+const h = date.getHours()
+const m = date.getMinutes()
 
 const quotes = [
 // current date/time
@@ -24,3 +24,16 @@ const quotesBirthday = [
     'All the best for your birthday.',
     'Have a very special day.'
 ];
+
+//debug outpus
+if(debugMode) {
+	console.log("name="+name)
+	console.log("birthday="+birthday)
+	console.log("date="+date)
+	console.log("debugMode="+debugMode)
+	console.log("numberOfBackgroundImages="+numberOfBackgroundImages)
+	console.log("date="+date)
+	console.log("h="+h)
+	console.log("quotes="+quotes)
+	console.log("quotesBirthday="+quotesBirthday)
+}
