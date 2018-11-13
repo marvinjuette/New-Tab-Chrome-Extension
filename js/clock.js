0a00e4bf6308ec4cb5edde50c267675cf4901526
Nivram710 <contact@nivram710.de>
2018-11-13 11:17:15 +0100

    Created own method to reformat time

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,32 @@
 // set the clock to the date const, clock will aperar instantly
 document.getElementById('clock').innerHTML = h + ":" + m
 
+h = reformatHours(h)
+m = reformatMiuntes(m)
+
+if(debugMode) console.log("h="+h)
+if(debugMode) console.log("m="+m)
+
+//reformat time 11:5 --> 11:05
+function reformatHours(hours) {
+	if(debugMode) {
+		console.log("reformatHours() called") 
+		console.log("hours="+hours)
+	}
+	if(hours < 10) return "0" + hours
+}
+
+function reformatMiuntes(minutes) {
+	if(debugMode) {
+		console.log("reformatMiuntes() called")
+		console.log("minutes="+minutes) 
+	}
+	if(minutes < 10) return "0" + minutes
+}
+
 // This functions shows the clock on the page
 function updateTime() {
-	if(debugMode) console.log("Debug: updateTime() called")	
+	if(debugMode) console.log("updateTime() called")	
 
 	let current_date = new Date()
 	let hours = current_date.getHours()
@@ -14,14 +37,8 @@ function updateTime() {
 		console.log("minutes=" + minutes)
 	}
 	
-	if(hours < 10) {
-		hours = "0" + hours
-		if(debugMode) console.log("hours: " + hours)
-	}
-	if(minutes < 10) {
-		minutes = "0" + minutes
-		if(debugMode) console.log("minutes: " + minutes)
-	}
+	hours = reformatHours(hours)
+	minutes = reformatMiuntes(minutes)
 	
 	document.getElementById('clock').innerHTML = hours + ":" + minutes
 }
